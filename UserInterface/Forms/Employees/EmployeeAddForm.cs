5c529308e2b6ef8871a5cd36b8c716afef11f27b
damprog <damprog.one@gmail.com>
2021-01-29 12:28:37 +0100

    40 series 11

--- UserInterface/Forms/Employees/EmployeeAddForm.cs
+++ UserInterface/Forms/Employees/EmployeeAddForm.cs
@@ -7,10 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SystemHR.DataAccessLayer.Model;
 using SystemHR.DataAccessLayer.Model.Dictionaries;
 using SystemHR.UserInterface.Extensions;
 using SystemHR.UserInterface.Forms.Base;
 using SystemHR.UserInterface.Forms.Employees.Base;
+using SystemHR.UserInterface.Helpers;
 
 namespace SystemHR.UserInterface.Forms.Employees
 {
@@ -68,8 +70,75 @@ namespace SystemHR.UserInterface.Forms.Employees
 
         protected override void Save()
         {
-            MessageBox.Show("Zapisano!");
-            Close();
+            if (ValidateForm())
+            {
+                EmployeeModel employee = new EmployeeModel()
+                {
+                    LastName = txtLastName.Text,
+                    FirstName = txtFirstName.Text,
+                    Gender = new GenderModel(cbGender.Text),
+                    DateBirth = dtpDateBirth.Value,
+                    PESEL = txtPesel.Text,
+                    PhoneNumber = txtPhoneNumber.Text,
+                    EmailAddress = txtEmailAddress.Text,
+                    IdentityCardNumber = txtIdentityCardNumber.Text,
+                    IssueDateIdentityCard = dtpIssueDateIdentityCard.Value,
+                    ExpirationDateIdentityCard = dtpExpirationDateIdentityCard.Value,
+                    PassportNumber = txtPassportNumber.Text,
+                    IssueDatePassport = dtpIssueDatePassport.Value,
+                    ExpirationDatePassport = dtpExpirationDatePassport.Value,
+                    Status = new StatusModel("Wprowadzony")
+                };
+
+                //employee = CreateEmployee(employee);
+
+                Close();
+            }
+            
+        }
+
+        private bool ValidateForm()
+        {
+            StringBuilder sbErrorMessage = new StringBuilder();
+
+            string lastNameErrorMessage = epLastName.GetError(txtLastName);
+            if(!string.IsNullOrEmpty(lastNameErrorMessage))
+            {
+                sbErrorMessage.Append(lastNameErrorMessage);
+            }
+
+            string firstNameErrorMessage = epFirstName.GetError(txtFirstName);
+            if (!string.IsNullOrEmpty(firstNameErrorMessage))
+            {
+                sbErrorMessage.Append(firstNameErrorMessage);
+            }
+
+            if (sbErrorMessage.Length > 0)
+            {
+                MessageBox.Show(
+                    sbErrorMessage.ToString(),
+                    "Dodawanie pracownika",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return false;
+            }
+
+            string peselWarningMessege = epPESEL.GetError(txtPesel);
+            if (!string.IsNullOrEmpty(peselWarningMessege))
+            {
+                DialogResult answer =
+                    MessageBox.Show(
+                        peselWarningMessege + Environment.NewLine + "Czy mimo to chcesz dodać pracownika",
+                        "Dodawanie pracownika",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Warning);
+
+                if (answer == DialogResult.No)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         protected override void Cancel()
@@ -103,7 +172,7 @@ namespace SystemHR.UserInterface.Forms.Employees
         {
             string pesel = txtPesel.Text;
 
-            if (!string.IsNullOrWhiteSpace(pesel))
+            if (!string.IsNullOrWhiteSpace(pesel) && !ValidatorsHelper.IsValidPESEL(pesel))
             {
                 epPESEL.SetError(txtPesel,"Cyfra kontrolna numeru pesel jest nieprawidłowa.");
             }
